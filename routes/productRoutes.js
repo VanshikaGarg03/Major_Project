50d468ae137470e19f78fce5bb902ea2943470ac
VanshikaGarg03 <vgarg4030@gmail.com>
2024-08-24 10:46:58 +0530

    first commit

---
+++ routes/productRoutes.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const Product = require('../models/Product');
+
+const router = express.Router();
+
+router.get('/', async (req, res) => {
+  try {
+    const products = await Product.find();
+    res.send(products);
+  } catch (error) {
+    res.status(500).send('Server error');
+  }
+});
+
+router.post('/', async (req, res) => {
+  try {
+    const { name, description, price } = req.body;
+    const product = new Product({ name, description, price });
+    await product.save();
+    res.status(201).send('Product added successfully');
+  } catch (error) {
+    res.status(500).send('Server error');
+  }
+});
+
+module.exports = router;
